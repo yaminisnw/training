3978a56883aacd8287075460cfc67bb5773184f9
yamini <yamini.s@nativewit.in>
2023-09-06 17:47:36 +0530

    First Command

---
+++ practise/statefulwidgets/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'list_of_strings.dart';
+import 'package:statefulwidgets/list_of_strings.dart';
+
+import 'ClickableApp.dart';
+
+void main() {
+  //runApp(const ClickApp());
+  runApp(const StringsApp());
+}
+
+//stringList
+
+/* void main()
+{
+  runApp(MaterialApp(home: StringsList()));
+}*/
+
